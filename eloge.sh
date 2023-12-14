8970cc92468bbf8b2bdfd0b58efe7cf2dd84f630
Philippe Jean Guillaumie <batden@sfr.fr>
2023-12-14 14:45:35 +0100

    Minor correction

--- eloge.sh
+++ eloge.sh
@@ -2,7 +2,7 @@
 
 # This script helps you cleanly and safely uninstall Enlightenment and related applications.
 
-# ELOGE is licensed under a Creative Commons Attribution-ShareAlike 4.0 International License,
+# ELOGE.SH is licensed under a Creative Commons Attribution-ShareAlike 4.0 International License,
 # in memory of Aaron Swartz.
 # See https://creativecommons.org/licenses/by-sa/4.0/
 
