8e9df72939559125a7744e27999a97193045acf5
Philippe Jean Guillaumie <batden@sfr.fr>
2023-12-30 06:48:02 +0100

    Remove comments

--- eloge.sh
+++ eloge.sh
@@ -299,8 +299,6 @@ uninstall_enlighten() {
   clear
   printf "\n\n$BLDR%s %s\n\n" "* UNINSTALLING ENLIGHTENMENT DESKTOP ENVIRONMENT *"
   sleep 1
-  printf "$BLDR%s %s\n\n" "This may take a few minutes."
-  sleep 1
   printf "$BLDR%s $OFF%s\n\n" "You will be prompted to answer some basic questions..."
   sleep 2
 
